c0d3c154f695263cc726f3a226632737b3561578
Palak Rawat <121319912+DeepeshSengarIO@users.noreply.github.com>
2023-01-14 04:10:25 +0200

    updated

--- weather-app/src/environments/environment.prod.ts
+++ weather-app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
+
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'weather-by-api-ninjas.p.rapidapi.com',
+
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'e73195a9camsh52d7f5c2367dbc7p181911jsn315b529d1a69'
+
 };
--- weather-app/src/environments/environment.ts
+++ weather-app/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+import 'zone.js/plugins/zone-error';
 export const environment = {
   production: false,
   weatherApiBaseUrl: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
